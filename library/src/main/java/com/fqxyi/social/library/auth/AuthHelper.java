2f787f13d1d336c8e6f9110f7ac1aa367c4fafb3
fqxyi <fengqingxiuyi@gmail.com>
2018-06-30 19:21:06 +0800

    解决一些问题

--- library/src/main/java/com/fqxyi/social/library/auth/AuthHelper.java
+++ library/src/main/java/com/fqxyi/social/library/auth/AuthHelper.java
@@ -12,14 +12,22 @@ import com.fqxyi.social.library.util.SocialUtil;
  */
 public class AuthHelper {
 
+    private static final int TYPE_AUTH_WX = 1;
+    private static final int TYPE_AUTH_QQ = 2;
+    private static final int TYPE_AUTH_WB = 3;
+
     private WXAuthHelper wxAuthHelper;
     private QQAuthHelper qqAuthHelper;
     private WBAuthHelper wbAuthHelper;
+
+    //当前授权类型
+    private int currAuthType;
 
     /**
      * 微信授权
      */
     public void authWX(Activity activity, IAuthCallback authCallback) {
+        currAuthType = TYPE_AUTH_WX;
         if (wxAuthHelper == null) {
             wxAuthHelper = new WXAuthHelper(activity, SocialUtil.get().getWxAppId(), SocialUtil.get().getWxAppSecret());
         }
@@ -30,6 +38,7 @@ public class AuthHelper {
      * QQ授权
      */
     public void authQQ(Activity activity, IAuthCallback authCallback) {
+        currAuthType = TYPE_AUTH_QQ;
         if (qqAuthHelper == null) {
             qqAuthHelper = new QQAuthHelper(activity, SocialUtil.get().getQqAppId());
         }
@@ -40,6 +49,7 @@ public class AuthHelper {
      * 微博授权
      */
     public void authWB(Activity activity, IAuthCallback authCallback) {
+        currAuthType = TYPE_AUTH_WB;
         if (wbAuthHelper == null) {
             wbAuthHelper = new WBAuthHelper(activity, SocialUtil.get().getWbAppId(), SocialUtil.get().getWbRedirectUrl());
         }
@@ -63,10 +73,10 @@ public class AuthHelper {
      * QQ授权和分享以及微博授权都需要在其当前的activity的onActivityResult中调用该方法
      */
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (qqAuthHelper != null) {
+        if (qqAuthHelper != null && currAuthType == TYPE_AUTH_QQ) {
             qqAuthHelper.onActivityResult(requestCode, resultCode, data);
         }
-        if (wbAuthHelper != null) {
+        if (wbAuthHelper != null && currAuthType == TYPE_AUTH_WB) {
             wbAuthHelper.onActivityResult(requestCode, resultCode, data);
         }
     }
